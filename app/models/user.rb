eb9ccd3f9f57193986bca532f2aca87193a91bea
Mo Amin <m.alamin92@gmail.com>
2015-10-20 00:33:39 -0400

    passed basic validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
-	validates :email, uniqueness: {case_senstive: false}
+	before_save {self.email = email.downcase}
+
+	validates :name, presence: true
+
+	validates :email, uniqueness: {case_sensitive: false}, presence: true,
+		length: {maximum: 25}
+
+	validates :password, length: {minimum: 6}, presence: true
+
+	validates :password_confirmation, presence: true
+
 	has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,40 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+  	@user = User.new(name: "Example User", email: "user@example.com", 
+  		password: "password", password_confirmation: "password")
+  end
+
+  test "should be valid" do
+  	assert @user.valid?
+  end
+
+  test "name should be present" do
+  	@user.name = "     "
+  	assert_not @user.valid?
+  end
+
+  test "email should be present" do
+  	@user.email = "    "
+  	@user.valid?
+  end
+
+  test "email should be unique" do
+  	diff_user = @user.dup
+  	diff_user.email = @user.email.upcase
+  	@user.save
+  	assert_not diff_user.valid?
+  end
+
+  test "password should be a certain length" do
+  	@user.password = "a" * 5
+  	assert_not @user.valid?
+  end
+
+  test "email should be a certain length" do
+  	@user.password = "a" * 25
+  	assert_not @user.valid?
+  end
+
 end
