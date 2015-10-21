e3e5bfe81af7ec9408b6c8b1de045e90f7bee8c8
Mo Amin <m.alamin92@gmail.com>
2015-10-21 00:58:03 -0400

    added login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			# login user
+			log_in(user)
+			flash[:success] = "Successfully loged in"
 			redirect_to user
 		else
 			flash.now[:danger] = "Invalid email/password combination."
