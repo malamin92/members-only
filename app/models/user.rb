0b25619fc8790714aa7834c18e7bf5e18baed073
Mo Amin <m.alamin92@gmail.com>
2015-10-22 03:24:19 -0400

    add checkbox to form view

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	def User.digest(string)
-		cost = ActiveModel::SecureaPassword.min_cost ? BCrypt::Engine::MIN_COST :
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 														BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
 	end
@@ -31,6 +31,11 @@ class User < ActiveRecord::Base
 	end
 
 	def authenticated?(remember_token)
+		return false if remember_digest.nil?
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)
 	end
 end
